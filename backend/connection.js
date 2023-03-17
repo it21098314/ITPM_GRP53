754fdcd007c36b737b636fa75154c562c3ca7003
it21098314 <it21098314@my.sliit.lk>
2023-03-17 14:12:25 +0530

    add feedback ,product, instructor files

---
+++ backend/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const connectionStr = `mongodb+srv://Admin123:ammadada@education.9ijcvve.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(connectionStr, {useNewUrlparser: true})
+.then(() => console.log('connected to mongodb'))
+.catch(err => console.log(err))
+
+mongoose.connection.on('error', err => {
+
+
+  
+  console.log(err)
+})
---
+++ backend/models/Insta.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let instaSchema = new Schema({
+  I_id: {
+    type : String,
+    required: true
+ },
+ I_name: {
+    type : String,
+    required: true
+ },
+ I_email: {
+     type : String,
+     required: true
+  },
+ I_address: {
+     type : String,
+     required: true
+  },
+  
+I_phone: {
+  type : String,
+  required: true
+},
+I_coll: {
+  type : String,
+  required: true
+},
+I_edu: {
+  type : String,
+  required: true
+}
+
+},
+
+{
+    collection: 'instas'
+  })
+
+module.exports = mongoose.model('Insta', instaSchema)
