d859f9d5a6cfc399ef2721807397349665c0c7fe
it21098314 <it21098314@my.sliit.lk>
2023-05-18 10:22:12 +0530

    Create categories.js

---
+++ src/categories.js
@@ -0,0 +1,16 @@
+const categories = [
+    {
+        name: "free",
+        img: "https://res.cloudinary.com/dvcfip5fe/image/upload/v1682064141/3_cv4odg.png",
+    },
+    {
+        name: "blog",
+        img: "https://res.cloudinary.com/dvcfip5fe/image/upload/v1681120426/blog_wrezzq.jpg",
+    },
+
+    { name: "paid", img: "https://res.cloudinary.com/dvcfip5fe/image/upload/v1682064176/4_hibsre.png" },
+];
+
+export default categories;
+//https://images.unsplash.com/photo-1518997554305-5eea2f04e384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80
+//https://images.unsplash.com/photo-1464380573004-8ca85a08751a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGhvbmV8ZW58MHx8MHx3aGl0ZXw%3D&auto=format&fit=crop&w=800&q=60
